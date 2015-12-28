8b7e0f972943d77506abc2bc74a34378f3328f37
Robert Pearce <me@robertwpearce.com>
2015-12-28 13:46:12 -0500

    using async/await for query util tests

--- mocha-babel.js
+++ mocha-babel.js
@@ -1,1 +1,2 @@
 require('babel-core/register');
+require('babel-polyfill');
--- src/db/queryUtils.test.js
+++ src/db/queryUtils.test.js
@@ -6,40 +6,37 @@ const name = 'Robert',
       network_id = 'b9dee03c-daf6-4656-9643-c93d78fe5758';
 
 describe('Query Utils', () => {
-  before((done) => {
-    destroy({ table: 'users' })
-      .then(() => {
-        find({ table: 'users' })
-          .then((users) => {
-            expect(users.length).to.equal(0);
-            done();
-          })
-          .catch(console.error);
-      })
-      .catch(console.error);
+  before(async (done) => {
+    try {
+      await destroy({ table: 'users' });
+      const users = await find({ table: 'users' });
+      expect(users.length).to.equal(0);
+      done();
+    } catch (err) {
+      console.error(err);
+    }
   });
 
   describe('create', () => {
-    it('creates a record in the DB and returns its id', (done) => {
-      create({ table: 'users', attrs: { name, email, network_id } })
-        .then((res) => {
-          expect(res[0].match(/-/g).length).to.equal(4); // simple UUID format test
-          find({ table: 'users' })
-            .then((users) => {
-              expect(users.length).to.equal(1);
-              done();
-            })
-            .catch(console.error);
-        })
-        .catch(console.error);
+    it('creates a record in the DB and returns its id', async (done) => {
+      try {
+        const res = await create({ table: 'users', attrs: { name, email, network_id } });
+        expect(res[0].match(/-/g).length).to.equal(4); // simple UUID format test
+        const users = await find({ table: 'users' });
+        expect(users.length).to.equal(1);
+        done();
+      } catch (err) {
+        console.error(err);
+      }
     });
 
-    it('catches errors from the DB', (done) => {
-      create({ table: 'users', attrs: { name: null, email, network_id } })
-        .catch((err) => {
-          expect(err.name).to.equal('error');
-          done();
-        });
+    it('catches errors from the DB', async (done) => {
+      try {
+        await create({ table: 'users', attrs: { name: null, email, network_id } });
+      } catch (err) {
+        expect(err.name).to.equal('error');
+        done();
+      }
     });
   });
 
